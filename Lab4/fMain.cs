fed4f67c7980a7ce8babff9b8fca74853c6ff0f2
vo992 <vo992@DESKTOP-2IIJ2BS>
2024-06-16 21:56:42 +0300

    Add project files.

---
+++ Lab4/fMain.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLib;
+using Lab3;
+
+namespace Lab4
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+            gvBicycles.AutoGenerateColumns = false;
+
+            DataGridViewColumn column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Model";
+            column.Name = "Модель";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Frame";
+            column.Name = "Рама";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Broadcast";
+            column.Name = "Передачі";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Fork";
+            column.Name = "Вилка";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewTextBoxColumn();
+            column.DataPropertyName = "Handlebars";
+            column.Name = "Руль";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewCheckBoxColumn();
+            column.DataPropertyName = "Ring";
+            column.Name = "Дзвінок";
+            gvBicycles.Columns.Add(column);
+
+            column = new DataGridViewCheckBoxColumn();
+            column.DataPropertyName = "Has3Wheels";
+            column.Name = "Має 3 колеса";
+            gvBicycles.Columns.Add(column);
+
+            bindSrcBicycles.Add(new Bicycle("Sport", "f12f", 124, "Fasflk", "asfqf", true, true));
+            EventArgs eventArgs = new EventArgs(); OnResize(eventArgs);
+        }
+
+        private void fMain_Resize(object sender, EventArgs e)
+        {
+            int buttonsSize = 5 * btnAdd.Width + 2 * toolStripSeparator1.Width + 30;
+            btnExit.Margin = new Padding(Width - buttonsSize, 0, 0, 0); 
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            Bicycle bicycle = new Bicycle();
+
+            fBicycle fbc = new fBicycle(bicycle);
+            if(fbc.ShowDialog() == DialogResult.OK)
+            {
+                bindSrcBicycles.Add(bicycle);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            Bicycle bicycle = (Bicycle)bindSrcBicycles.List[bindSrcBicycles.Position];
+
+            fBicycle fbc = new fBicycle(bicycle);
+            if (fbc.ShowDialog() == DialogResult.OK)
+            {
+                bindSrcBicycles.List[bindSrcBicycles.Position] = bicycle;
+            }
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Видалити поточний запис?", "Видалення запису", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning) == DialogResult.OK)
+            { bindSrcBicycles.RemoveCurrent(); }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("Очистити таблицю?\n\nВсі дані будуть втрачені", "Очищення даних", MessageBoxButtons.OKCancel, MessageBoxIcon.Question)== DialogResult.OK)
+                bindSrcBicycles.List.Clear();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Закрити застосунок?", "Вихід з програми", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
